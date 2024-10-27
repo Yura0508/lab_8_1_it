43317d02e28958e19481f30d4f75f5d972ed1678
Yura <urijkisil2007@gmail.com>
2024-10-27 20:15:17 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_1_it/lab_8_1_it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestCount)
+        {
+            char testStr1[] = "This is a while test while and another while.";
+            char cs[] = "while";
+            Assert::AreEqual(3, Count(testStr1, cs));
+
+            char testStr2[] = "No matches here.";
+            Assert::AreEqual(0, Count(testStr2, cs));
+
+
+            char testStr4[] = "whilewhilewhile";
+            Assert::AreEqual(3, Count(testStr4, cs));
+        }
+
+        // Тестуємо функцію Change
+        TEST_METHOD(TestChange)
+        {
+
+            char testStr2[] = "No while here.";
+            char expectedOutput2[] = "No *** here.";
+            Change(testStr2); // Використовуємо самозмінювальний рядок
+            Assert::AreEqual(expectedOutput2, testStr2);
+
+            char testStr3[] = "while while while";
+            char expectedOutput3[] = "*** *** ***";
+            Change(testStr3); // Використовуємо самозмінювальний рядок
+            Assert::AreEqual(expectedOutput3, testStr3);
+        }
+
+	};
+}
---
+++ lab_8_1_it/lab_8_1_it.cpp
@@ -0,0 +1,79 @@
+﻿// lab_8_1_it.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(const char* s, const char* cs)
+{
+	int count = 0;
+	int j, k;
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		k = i;
+		while (cs[j] != '\0')
+		{
+			if (s[k] == cs[j] && s[k] != '\0')
+			{
+				j++;
+				k++;
+			}
+			else
+				break;
+		}
+		if (cs[j] == '\0')
+			count++;
+	}
+	return count;
+}
+
+char* Change(char* str)
+{
+	size_t len = strlen(str);
+	if (len < 5)
+		return str;
+	char* tmp = new char[len * 3.0 / 5 + 1];
+	char* t = tmp;
+	tmp[0] = '\0';
+	size_t i = 0;
+	while (i < len && str[i + 4] != 0)
+	{
+		if (str[i] == 'w' && str[i + 1] == 'h' && str[i + 2] == 'i' && str[i + 3] == 'l' && str[i + 4] == 'e')
+		{
+			strcat(t, "***");
+			t += 3;
+			i += 5;
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+		}
+	}
+	*t++ = str[i++];
+	*t++ = str[i++];
+	*t++ = str[i++];
+	*t++ = str[i++];
+	*t = '\0';
+	strcpy(str, tmp);
+	return tmp;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	char cs[] = "while";
+	cout << Count(str, cs) << endl;
+
+	char* dest = new char[101];
+	dest = Change(str);
+
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	cin.get();
+	return 0;
+}
